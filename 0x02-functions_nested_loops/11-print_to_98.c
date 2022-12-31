3f156f66dd7e470ec464117c30f98cb6878d74f5
amsungsong <amsungsong@gmail.com>
2022-12-30 22:45:07 -0800

    Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,5 @@
-#include "main.h" 
+#include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
@@ -8,7 +7,7 @@
  * Return: Always 0.
  */
 void print_to_98(int n)
-{ 
+{
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
