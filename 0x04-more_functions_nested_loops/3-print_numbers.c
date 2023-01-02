e868d43bc665f991306265de9cdf86eca3a3cf23
amsungsong <amsungsong@gmail.com>
2023-01-02 08:11:07 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main h"
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+while (i < 10)
+{
+_putchar (i + '0');
+I++;
+}
+_putchar ('\n');
+}
