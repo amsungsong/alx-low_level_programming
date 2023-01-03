703a31ffa0044319acff9e622d4b9883870107fa
amsungsong <amsungsong@gmail.com>
2023-01-03 08:41:05 -0800

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,15 @@
 #include "main.h"
+
 /**
  * _strcmp - compare 2 string
- * @s1:string
- * @s2:strmp
- * Return:int
+ * @s1: first string
+ * @s2: second string 
+ * Return
+ *         returns zero if s1 == s2
+ *         returns negative number if s1 < s2
+ *         returns positive number if s1 > s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
