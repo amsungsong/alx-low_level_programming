05e6b24fac2943f937605abf125fb2f0c151bedd
amsungsong <amsungsong@gmail.com>
2023-01-03 08:14:02 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
