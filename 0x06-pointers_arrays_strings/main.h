ddb92a53da12735eab1aa8872d26896bb35e2b85
amsungsong <amsungsong@gmail.com>
2023-01-03 22:37:08 -0800

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *str);
-char *leet(char *);
+char *leet(char *a);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
